cca7972265366255351ac63f4a23a6d7ce8a8726
kranthimj23 <kranthimj23@gmail.com>
2025-06-20 17:58:16 +0530

    added files

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "healthy"})
+
+@app.route('/ready')
+def ready():
+    return jsonify({"status": "ready"})
+
+@app.route('/write')
+def write():
+    return jsonify({"msg": "Data written to Aerospike and PostgreSQL"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
